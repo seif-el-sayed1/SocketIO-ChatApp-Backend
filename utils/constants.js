76597f7267ae1986e957b1536ea0c37761febb07
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-02 21:31:13 +0200

    constants

---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+exports.USER = "user";
+
+exports.ROLES = [exports.USER];
+
+exports.LOGIN_TYPE_LIST = ["apple", "google", "email", "social"];
+exports.LOGIN_TYPE_PLATFORM_LIST = ["apple", "google", "social"];
+exports.GENDER_LIST_EN = ["male", "female"];
+exports.GENDER_LIST_AR = ["ذكر", "أنثى", "أنثي", "انثي", "انثى"];
+
+// Media types
+exports.MEDIA_MESSAGE_TYPES = ["image", "video", "audio", "file"];
+exports.LABEL = "Label";
+exports.MESSAGE_TYPES = ["text", exports.LABEL, ...exports.MEDIA_MESSAGE_TYPES];
