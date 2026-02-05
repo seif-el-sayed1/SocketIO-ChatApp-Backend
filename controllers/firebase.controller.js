948dd90f8cb34705c2a5b33ee7fdac30eaaa93a7
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-06 00:39:47 +0200

    update firebase

--- controllers/firebase.controller.js
+++ controllers/firebase.controller.js
@@ -56,70 +56,6 @@ const uploadDocumentAndGetUrl = async (document, baseName, innerName) => {
   return await getDownloadURL(snapshot.ref);
 };
 
-
-/**
- * Uploads the given video to Firebase Storage and returns the download URL
- * of the uploaded video
- * @param {Buffer} video The video to be uploaded
- * @returns {Promise<string>} The download URL of the uploaded video
- */
-const uploadVideoAndGetUrl = async (video) => {
-    // Upload video
-    const storageRef = ref(
-        storage,
-        `${folderName}/video-${uuidv4()}-${Date.now()}-${folderName.toLowerCase()}.mp4`
-    );
-    const metadata = { contentType: "video/mp4" };
-    const snapshot = await uploadBytesResumable(storageRef, video.buffer, metadata);
-    return await getDownloadURL(snapshot.ref);
-};
-
-const uploadVoiceAndGetUrl = async (voice) => {
-    const storageRef = ref(
-        storage,
-        `${folderName}/voice-${uuidv4()}-${Date.now()}-${folderName.toLowerCase()}.mp3`
-    );
-    const metadata = { contentType: "audio/mpeg" };
-    const snapshot = await uploadBytesResumable(storageRef, voice.buffer, metadata);
-    return await getDownloadURL(snapshot.ref);
-};
-
-/**
- * Deletes a video from Firebase Storage using its URL.
- *
- * @param {string} fileUrl - The URL of the video to be deleted.
- * @param {string} folderName - The name of the folder where the video is stored.
- * @returns {Promise<boolean>} - Returns true if the video is successfully deleted, otherwise false.
- */
-const deleteVideoByUrl = async (fileUrl, folderName) => {
-    try {
-        const fullPath = new URL(fileUrl).pathname;
-        const startIndex = fullPath?.indexOf("video-");
-        const fileName = fullPath.substring(startIndex);
-        const storageRef = ref(storage, `${folderName}/${fileName}`);
-        await deleteObject(storageRef);
-        return true;
-    } catch (error) {
-        console.log("🚀 ~ deleteVideoByUrl ~ error:", error);
-        return false;
-    }
-};
-
-const deleteVoiceByUrl = async (fileUrl, folderName) => {
-    try {
-        const fullPath = new URL(fileUrl).pathname;
-        const startIndex = fullPath?.indexOf("voice-");
-        const fileName = fullPath.substring(startIndex);
-        const storageRef = ref(storage, `${folderName}/${fileName}`);
-        await deleteObject(storageRef);
-        return true;
-    } catch (error) {
-        console.log("🚀 ~ deleteVoiceByUrl ~ error:", error);
-        return false;
-    }
-};
-
-
 /**
  * Deletes an image from Firebase Storage using its URL.
  *
@@ -211,125 +147,6 @@ class FirebaseImageController {
       next();
     });
 
-  uploadMultipleImages = (modelName) =>
-    asyncHandler(async (req, res, next) => {
-      folderName = modelName;
-      const { files } = req;
-      let fieldName;
-      if (files && files.length > 0) {
-        const photosByField = {}; 
-        await Promise.all(
-            files.map(async (file) => {
-                if (file.fieldname?.toLowerCase()?.includes("images")) {
-                    if (!photosByField[file.fieldname]) photosByField[file.fieldname] = [];
-                    photosByField[file.fieldname].push(await uploadImageAndGetUrl(file));
-                }
-            })
-        );
-
-        for (const key in photosByField) {
-            req.body[key] = photosByField[key];
-        }
-    }
-      next();
-  });
-
-  uploadSingleImage = (modelName) =>
-    asyncHandler(async (req, res, next) => {
-      folderName = modelName;
-      const files = req.files;
-      const file = req.file;
-      if (!files && !file) return next(); // if image is not sent then skip this middleware (user image is optional)
-      if (req.files?.length > 0) {
-        return next();
-      } else if (file) req.body.image = await uploadImageAndGetUrl(file);
-      next();
-    });
-
-  uploadFile = (modelName) =>
-    asyncHandler(async (req, res, next) => {
-      folderName = modelName;
-      const file = req.file;
-      // console.log("in upload image=>", file);
-      if (!file) return next(); // if image is not sent then skip this middleware (user image is optional)
-      req.body.document = await uploadDocumentAndGetUrl(
-        file,
-        req.body.entityName,
-        req.body.ownerName
-      );
-      next();
-      return;
-    });
-
-  uploadMultipleVideos = (modelName) =>
-    asyncHandler(async (req, res, next) => {
-        folderName = modelName;
-        const { files } = req;
-        let fieldName;
-
-        if (files && files.length > 0) {
-            let videosPromise = [];
-
-            await Promise.all(
-                files.map(async (file) => {
-                    if (file.fieldname?.toLowerCase()?.includes("videos")) {
-                        fieldName = file.fieldname;
-                        console.log(fieldName)
-                        videosPromise.push(uploadVideoAndGetUrl(file));
-                    }
-                })
-            );
-
-            if (videosPromise.length > 0) {
-                req.body[fieldName] = await Promise.all(videosPromise);
-            }
-        }
-
-        next();
-    });
-
-  uploadMultipleVoices = (modelName) =>
-    asyncHandler(async (req, res, next) => {
-        folderName = modelName;
-        const { files } = req;
-        let fieldName;
-
-        if (files && files.length > 0) {
-            let voicesPromise = [];
-
-            await Promise.all(
-                files.map(async (file) => {
-                    if (file.fieldname?.toLowerCase()?.includes("voices")) {
-                        fieldName = file.fieldname;
-                        voicesPromise.push(uploadVoiceAndGetUrl(file));
-                    }
-                })
-            );
-
-            if (voicesPromise.length > 0) {
-                req.body[fieldName] = await Promise.all(voicesPromise);
-            }
-        }
-
-        next();
-    });
-
-
-
-    uploadSingleVideo = (modelName, fieldName) =>
-        asyncHandler(async (req, res, next) => {
-            folderName = modelName;
-            const file = req.file;
-            console.log(file)
-            if (!file) return next();
-            req.body.stadiumVideos = await uploadVideoAndGetUrl(file);
-            next();
-        });
-
-    deleteOldVideo = async (video, folderName) => await deleteVideoByUrl(video, folderName);
-
-    deleteOldVoice = async (voice, folderName) => await deleteVoiceByUrl(voice, folderName);
-
   /**
    *
    * Deletes Image By URL And folderName
@@ -338,35 +155,34 @@ class FirebaseImageController {
    * @returns true or false
    */
   deleteOldImage = async (image, folderName) => await deleteImageByUrl(image, folderName);
+  // Chat media
+  uploadMultipleImagesForTheChat = (modelName) =>
+    asyncHandler(async (req, res, next) => {
+      const lang = req.headers.lang || "en";
+      folderName = `${modelName}`;
+      const { files } = req;
+      if (!files) return next(new ApiError(translate("Media file is required", lang), 404));
+      if (files.length > 0) {
+        let urls = [];
+        for (let i = 0; i < files.length; i++) {
+          let file = files[i];
+          urls.push(uploadImageAndGetUrl(file));
+        }
+        req.body.media = await Promise.all(urls);
+      }
+      next();
+    });
 
-  /**
-   *
-   * Deletes Document By URL And folderName
-   * @param {String} document a url link to be deleted
-   * @param {String} folderName the folder where the image is saved on firebase
-   * @returns true or false
-   */
-  deleteOldDocument = async (document, folderName) => await deleteDocumentUrl(document, folderName);
-
-  rollbackNewUserImage = asyncHandler(
-    async (req, res, next) => await deleteImageByUrl(req.body.image)
-  );
-
-  // Notifications
-  addNotificationImage = asyncHandler(async (req, res, next) => {
-    folderName = "Notification";
-    if (req.file) req.body.image = await uploadImageAndGetUrl(req.file);
-    return;
+  rollbackChatImages = asyncHandler(async (req) => {
+    const { media } = req.body;
+    if (media && media.length > 0) {
+      let promises = [];
+      for (let image of media) {
+        promises.push(deleteImageByUrl(image, folderName));
+      }
+      await Promise.all(promises);
+    }
   });
-
-  deleteOldNotificationImage = asyncHandler(
-    async (req) => await deleteImageByUrl(req.oldImage, "Notification")
-  );
-
-  rollbackNotificationImage = asyncHandler(
-    async (req) => await deleteImageByUrl(req.body.image, "Notification")
-  );
-
 }
 
 module.exports = new FirebaseImageController();
