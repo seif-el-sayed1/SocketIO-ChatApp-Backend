1e35b88a0d7253683317779c9e1707a1477c8bee
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-07 02:44:07 +0200

    block, unblock, change lang routes

---
+++ routes/actions.routes.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+// Auth middlewares
+const { protect, allowedTo } = require("../middlewares/auth.middleware");
+
+// Constants
+const { USER } = require("../utils/constants");
+
+// Classes
+const ActionsController = require("../controllers/actions.controller");
+const UserValidator = require("../validators/user.validator");
+const GlobalValidator = require("../validators/global.validator");
+// Router
+const router = express.Router();
+
+// Routes
+
+router
+  .route("/lang")
+  .patch(
+      protect,
+      allowedTo(USER),
+      GlobalValidator.updateUserLangValidator,
+      UserController.updateUserLang
+  );
+
+router
+  .route("/:id/block")
+  .post(
+    protect,
+    allowedTo(USER),
+    UserValidator.validateUserBlock,
+    ActionsController.blockUser
+  );
+
+router
+  .route("/:id/unblock")
+  .post(
+    protect,
+    allowedTo(USER),
+    UserValidator.validateUserBlock,
+    ActionsController.unBlockUser
+  );
+
+module.exports = router;
