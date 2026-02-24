cdd51416152a81f546f068e7d743aa257139d565
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-24 15:22:25 +0200

    refactor: update Firebase initialization to use environment variables for credentials

--- startup/firebase.js
+++ startup/firebase.js
@@ -1,28 +1,34 @@
 const { initializeApp } = require("firebase/app");
 const { getStorage } = require("firebase/storage");
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebaseServiceAccountKey.json");
+
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 });
 
 const decodeToken = async (idToken) => {
-  const userData = await admin.auth().verifyIdToken(idToken);
-  return userData;
+    const userData = await admin.auth().verifyIdToken(idToken);
+    return userData;
 };
 
 const storage = getStorage(app);
+
 module.exports = { storage, decodeToken };
